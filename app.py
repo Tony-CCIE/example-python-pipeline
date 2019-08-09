ebaaed429a0fd41d8e23ed311e064fb58e9977bc
Tony-CCIE <1278146756@qq.com>
2019-08-09 16:11:02 +0800

    app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return "Hello, kubernetes! ----\n"
+    return "Hello, kubernetes! CI/CD\n"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
